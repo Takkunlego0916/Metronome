04b93bbeca3febcbb341b5355f6449e97e7dc685
TakkunMC_jp <Kaneda.family.803@gmail.com>
2026-04-19 12:56:39 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,11 +7,36 @@ function createWindow() {
     height: 700,
     autoHideMenuBar: true,
     icon: path.join(__dirname, "icon.ico"),
-    webPreferences: { contextIsolation: true }
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+      sandbox: true,
+      webSecurity: true,
+      allowRunningInsecureContent: false
+    }
   });
 
-  win.setMenuBarVisibility(false);
-  win.loadFile("index.html");
+  win.removeMenu();
+  win.loadFile(path.join(__dirname, "index.html"));
+
+  win.webContents.setWindowOpenHandler(() => ({ action: "deny" }));
+  win.webContents.on("will-navigate", (event) => {
+    event.preventDefault();
+  });
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
